0b8cc9b41061414cf1908029866401ee5356770e
bikebroken <420936225@qq.com>
2020-03-09 23:29:21 +0800

    add sub module

---
+++ aa-mapper/src/main/java/com/carbon/approvalassistant/mapper/AaMapperApplication.java
@@ -0,0 +1,13 @@
+package com.carbon.approvalassistant.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaMapperApplication.class, args);
+    }
+
+}
